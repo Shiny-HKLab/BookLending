51879ff0dbffec1df45278ea25d8dc962b9c5dfc
onikarubi <printuserselect@gmail.com>
2024-12-16 17:03:32 +0900

    first commit.

---
+++ src/main.py
@@ -0,0 +1,5 @@
+from models.person import Person
+
+if __name__ == "__main__":
+    person = Person(first_name="John", last_name="Doe")
+    print(person)
---
+++ src/models/person.py
@@ -0,0 +1,7 @@
+import dataclasses
+
+
+@dataclasses.dataclass
+class Person:
+    first_name: str
+    last_name: str
